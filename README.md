09e98be3f5162094f2e984348f2965d757652ede
Eli Pandolfo <epandolf@ucsc.edu>
2018-03-13 22:18:10 -0700

    added sliders and fixed bugs

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # A model orrery, displaying the sun and the planets rotating around it
 
-This project is incomplete. When finished, there will be a slider to adjust the scale and one to adjust the speed at which time passes in the model. 
+All planets will feature their correct sizes, distances from each other, rotational period, and orbital period.
 
-All planets will be feature their correct sizes, distances from each other, rotational period, and orbital period, as well as the eccentricity of their orbits.
-
-The sun is far smaller and closer to the planets than it is in real life so that I can fit everything onto one window.
+The sun is far smaller and closer to the planets than it is in real life so that I can fit everything into one window.
 This presents an issue because Jupiter is now larger than the sun.
 
 I did this project to increase my skill with web apps and JS. I opted to use pure JavaScript for everything, because I want to get good with plain JS before focusing on add-ons. 
 
 If you want to see how it looks for real, simply clone the repo then open the html file locally in a browser. I like using chrome on full screen mode since you don't get the search bar and can use the entire screen to see the orrery.
 
-To change the time or scale, edit the unit or time variables in the solar_system.js.
+To change the time or scale, adjust the respective sliders.
 
 Note: I do not own the rights to any of these pictures. They were all copied from google images.
+
+# License
+
+Copyright <2018> <Eli Pandolfo>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- solar_system.js
+++ solar_system.js
@@ -1,35 +1,27 @@
 /*
+    solar_system.js
     scripts for solar system animation
     Eli Pandolfo
 
-    to ensure accuracy, all diameters, distances, rotational periods, and orbital periods
-    are all accurate, and are scaled to the size of the model when they are rendered.
-
-    The only exception to this is the sun, because if the sun were small enough
-    that the other planets were displayed in the window, they would all be less
-    than 1 pixel in diameter. The sun is smaller than it should be by a factor of
-    X, and all planets are closer to the sun than they should be by a factor of Y.
-
-    FILL X AND Y
-
-    file:///Users/Eli/Desktop/coding/js/solar_system.html
+    // the sun is way smaller and closer to the planets than it is in real life, in order
+    // to be able to see the sun on the screen along with anything else.
+    // Otherwise, all the planets are their correct scaled distances from each other,
+    // are the correct size, and rotate and revolve at the correct angular velocities.
 */
 
 // static vars and properties: initial locations and orbits
 
 // unit = the standard unit of distance
 // all widths and distances are in terms of unit
-var unit = .001
+var unit
+unit = .0001
 
 // time is the rate at which time passes in this universe.
 // in our world, time moves at roughly one second per second
-var time = 10
-
-// eventually, unit and time will be controlled by sliders.
+var time = 1
 
 // each body is an object with features
-// diameter is in terms of unit, and is 
-// a multiple of the planet's actual diameter in km
+// diameter is a multiple of the planet's actual diameter in km
 
 // top and left are always the exact center of the window
 // adjusted so that the center of the planet is centered
@@ -40,90 +32,88 @@ var time = 10
 // the orbital's minor axis in km,
 // and the orbital period in earth days
 // distances from the sun are scaled by a factor of .001
+
+// the sun is way smaller and closer to the planets than it is in real life, in order
+// to be able to see the sun on the screen along with anything else.
+// Otherwise, all the planets are their correct scaled distances from each other,
+// are the correct size, and rotate and revolve at the correct angular velocities.
 var sun = 
 {
-    diameter: 100000.0 * unit, //this is not accurate
-    top: window.innerHeight/2.0 - (100000.0/2.0 * unit),
-    left: window.innerWidth/2.0 - (100000.0/2.0 * unit),
-    eq: null,
+    diameter: 100000.0,
+    eq: [0, 0, 1],
     rot_period: -3000,
     id: 'sun'
 }
 var mercury = 
 {
-    diameter: 4789.0 * unit,
-    top: window.innerHeight/2.0 - (4789.0/2.0 * unit),
-    left: window.innerWidth/2.0 - (4789.0/2.0 * unit),
-    eq: [57.9 * 10**3 * unit, 57.9 * 10**3 * unit, 88.0],
+    diameter: 4789.0,
+    eq: [57.9 * 10**3, 57.9 * 10**3, 88.0],
     rot_period: 1407.6,
     id: 'mercury'
 }
 var venus =
 {   
-    diameter: 12104.0 * unit,
-    top: window.innerHeight/2.0 - (12104.0/2.0 * unit),
-    left: window.innerWidth/2.0 - (12104.0/2.0 * unit),
-    eq: [108.2 * 10**3 * unit, 108.2 * 10**3 * unit, 224.7],
+    diameter: 12104.0,
+    eq: [108.2 * 10**3, 108.2 * 10**3, 224.7],
     rot_period: -5832.5,
     id: 'venus'
 }
 var earth = 
 {
-    diameter: 12756.0 * unit,
-    top: window.innerHeight/2.0 - (12756.0/2.0 * unit),
-    left: window.innerWidth/2.0 - (12756.0/2.0 * unit),
-    eq: [149.6 * 10**3 * unit, 149.6 * 10**3 * unit, 365.2],
+    diameter: 12756.0,
+    eq: [149.6 * 10**3, 149.6 * 10**3, 365.2],
     rot_period: 23.9,
     id: 'earth'
 }
 var mars = 
 {
-    diameter: 6792.0 * unit,
-    top: window.innerHeight/2.0 - (6792.0/2.0 * unit),
-    left: window.innerWidth/2.0 - (6792.0/2.0 * unit),
-    eq: [227.9 * 10**3 * unit, 227.9 * 10**3 * unit, 687.0],
+    diameter: 6792.0,
+    eq: [227.9 * 10**3, 227.9 * 10**3, 687.0],
     rot_period: 24.6,
     id: 'mars'
 }
 var jupiter = 
 {
-    diameter: 142984.0 * unit,
-    top: window.innerHeight/2.0 - (142984.0/2.0 * unit),
-    left: window.innerWidth/2.0 - (142984.0/2.0 * unit),
-    eq: [778.6 * 10**3 * unit, 778.6 * 10**3 * unit, 4331.0],
+    diameter: 142984.0,
+    eq: [778.6 * 10**3, 778.6 * 10**3, 4331.0],
     rot_period: 9.9,
     id: 'jupiter'
 }
 var saturn =
 {
-    diameter: 120536.0 * unit,
-    top: window.innerHeight/2.0 - (120536.0/2.0 * unit),
-    left: window.innerWidth/2.0 - (120536.0/2.0 * unit),
-    eq: [1433.5 * 10**3 * unit, 1433.5 * 10**3 * unit, 10747.0],
+    diameter: 120536.0,
+    eq: [1433.5 * 10**3, 1433.5 * 10**3, 10747.0],
     rot_period: 10.7,
     id: 'saturn'
 }
 var uranus = 
 {
-    diameter: 51118.0 * unit,
-    top: window.innerHeight/2.0 - (51118.0/2 * unit),
-    left: window.innerWidth/2.0 - (51118.0/2 * unit),
-    eq: [2782.5 * 10**3 * unit, 2782.5 * 10**3 * unit, 30589.0],
+    diameter: 51118.0,
+    eq: [2782.5 * 10**3, 2782.5 * 10**3, 30589.0],
     rot_period: 17.2,
     id: 'uranus'
 }
 var neptune =
 {
-    diameter: 49528.0 * unit,
-    top: window.innerHeight/2.0 - (49528.0/2 * unit),
-    left: window.innerWidth/2.0 - (49582.0/2 * unit),
-    eq: [4495.1 * 10**3 * unit, 4495.1 * 10**3 * unit, 59800.0],
+    diameter: 49528.0,
+    eq: [4495.1 * 10**3, 4495.1 * 10**3, 59800.0],
     rot_period: 16.1,
     id: 'neptune'
 }
 
-//planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+// slider calls this function to resize the dispplay
+function resize()
+{
+    unit = .00001 * document.getElementById('size_slider').value
+}
 
+// slider calls this funciton to speed up and slow down time
+function retime()
+{
+    time = 2**(document.getElementById('time_slider').value / 100) - 1
+}
+
+// rotates a body about its center
 function rotate(body) // can be the sun or planets
 {
     var angle = 0
@@ -140,6 +130,8 @@ function rotate(body) // can be the sun or planets
     }
 }
 
+// revolves a body about the sun
+// the sun uses this function even though it is not a planet
 function plot_path(planet)
 {
     var angle = 0
@@ -152,26 +144,16 @@ function plot_path(planet)
             angle = 0
         else
         {
-            var x = planet.eq[0] * Math.cos(angle)
-            var y = planet.eq[1] * Math.sin(angle)
+            var x = planet.eq[0] * Math.cos(angle) * unit
+            var y = planet.eq[1] * Math.sin(angle) * unit
 
-            // d stands for displayed
             var d = document.getElementById(planet.id)
-            d.style.top = y + planet.top + 'px'
-            d.style.left = x + planet.left + 'px'
-            d.style.width = planet.diameter + 'px'
-            d.style.height = planet.diameter + 'px'
+            d.style.top = y + window.innerHeight / 2 - (planet.diameter / 2 * unit) + 'px'
+            d.style.left = x + window.innerWidth / 2 - (planet.diameter / 2 * unit) + 'px'
+            d.style.width = planet.diameter * unit + 'px'
+            d.style.height = planet.diameter * unit + 'px'
             angle += 360 / planet.eq[2] * time * .001
         }
     }
 }
 
-function init_sun()
-{
-    var d_sun = document.getElementById(sun.id)
-    d_sun.style.top = sun.top + 'px'
-    d_sun.style.left = sun.left + 'px'
-    d_sun.style.width = sun.diameter + 'px'
-    d_sun.style.height = sun.diameter + 'px'
-    rotate(sun)
-}
